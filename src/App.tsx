bfde51d9cc261f9e3c1aaa64a90bcaf8e8c5bbb1
RichMix <richie.habib@yahoo.com>
2023-10-09 12:59:22 -0700

    Added count log in console

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log(count)
 
   return (
     <>
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>WHAT'S ASDFASFUP????</h1>
+      <h1>Ready To Mint That Piggy?</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
