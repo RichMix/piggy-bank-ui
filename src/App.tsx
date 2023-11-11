928145f5895395245ed3e0945f0190a7ff8409b8
lza_menace <lza_menace@protonmail.com>
2023-11-10 19:59:00 -0800

    update code with boilerplate mint button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import { useState } from 'react'
+import React, { useState, useCallback, FC, useMemo } from 'react'
 import reactLogo from './assets/media/PiggyGif.gif'
-import viteLogo from '/vite.svg'
-import backgroundImage from './assets/media/pig-bg/pg.png'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+import '@solana/wallet-adapter-react-ui/styles.css'
+
 import './App.css'
 
 function App() {
@@ -10,34 +20,63 @@ function App() {
 
   const [minting, setMinting] = useState(false);
 
-  const handleMint =async () => {
+  const handleMint = async () => {
     setMinting(true);
     console.log('Minting the Piggy Bank NFT');
     setMinting(false);
   }
 
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Mainnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+      () => [
+          /**
+           * Wallets that implement either of these standards will be available automatically.
+           *
+           *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
+           *     (https://github.com/solana-mobile/mobile-wallet-adapter)
+           *   - Solana Wallet Standard
+           *     (https://github.com/solana-labs/wallet-standard)
+           *
+           * If you wish to support a wallet that supports neither of those standards,
+           * instantiate its legacy wallet adapter here. Common legacy adapters can be found
+           * in the npm package `@solana/wallet-adapter-wallets`.
+           */
+          new UnsafeBurnerWalletAdapter(),
+      ],
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [network]
+  );
+
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Ready To Mint That Piggy?</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Mint site currently in development. Please be patient.
-        </p>
-      </div>
       <p className="read-the-docs">
         Please connect your wallet to mint a pig.
       </p>
+      <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect>
+              <WalletModalProvider>
+                  <WalletMultiButton />
+                  <WalletDisconnectButton />
+                  { /* Your app's components go here, nested within the context providers. */ }
+              </WalletModalProvider>
+          </WalletProvider>
+      </ConnectionProvider>
+      <div className="">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
     </>
   )
 }
